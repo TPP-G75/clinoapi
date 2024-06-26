092446de79b3008433963dbbbb5ae0213b951689
juan roussilian <juancruzroussilian9@gmail.com>
2024-06-26 00:33:38 -0300

    feat: Add endpoint to extract medical terms from clinical note using llama2 llm model with prompt template

--- src/main.py
+++ src/main.py
@@ -2,25 +2,56 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import uvicorn
 import spacy
+import ollama
+
 
 class Note(BaseModel):
     text: str
 
+
 app = FastAPI()
 nlp = spacy.load("es_core_news_md")
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/notes")
 def analyze_note(note: Note):
     doc = nlp(note.text)
     entities = [
-        {"text": ent.text, "start": ent.start_char, "end": ent.end_char, "label": ent.label_}
+        {
+            "text": ent.text,
+            "start": ent.start_char,
+            "end": ent.end_char,
+            "label": ent.label_,
+        }
         for ent in doc.ents
     ]
     return {"Entities": entities}
 
+
+@app.post("/llm")
+def extract_terms(note: Note):
+    prompt_template = f""" Eres un medico experto con capacidad de reconocer terminos medicos
+    Extrae del siguiente texto, las palabras o oraciones cortas que representen terminos
+    medicos que representen sintomas, malestares, condiciones medicas, tratamientos,
+    etcetera, y los retornes como items de una lista de 10 items.
+
+    No agregues una expliacion de los terminos extraidos y no incluyas otras palabras
+    que no sean los terminos extraidos en la respuesta. La respuesta solo debe
+    contener la lista  sin numerar Además, no incluyas palabras que sean sinónimos
+    o representen la misma patología, y la respuesta debe  estar en español
+
+    {note.text}"""
+
+    response = ollama.generate(model="llama2", prompt=prompt_template)["response"]
+
+    # TODO: send to the snostorm api the response of the model to get the snomed codes
+    return {"response": response}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
